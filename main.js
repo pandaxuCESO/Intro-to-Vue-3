ef5fc1e58c179dc8f5bbda674a461357a61f075c
Panda Xu <panda.xu@theceso.com>
2021-06-01 13:45:21 -0500

    finsihed L4

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+// This is a Vue app's data object
 const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            inventory: 10,
+            onSale: true
         }
     }
 })
